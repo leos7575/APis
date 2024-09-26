06a441affcc327e9a1638ad26ee14d80cc6e5196
leos7575 <leostar7575@gmail.com>
2024-09-26 11:51:27 -0600

    nose2

--- maps.js
+++ maps.js
@@ -1,11 +1,27 @@
+
+// Initialize and add the map
 let map;
 
 async function initMap() {
+  // The location of Uluru
+  const position = { lat: 21.8141671, lng: -102.7697579 };
+  // Request needed libraries.
+  //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
 
+  // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    center: { lat: 21.8141671, lng: -102.7697579 },
     zoom: 15,
+    center: position,
+    mapId: "1",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
   });
 }
 
